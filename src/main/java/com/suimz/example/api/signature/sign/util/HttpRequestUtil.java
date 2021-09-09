00c4ec9e3660c12727269a5c1f19424f702b5e48
穗苗子 <994599447@qq.com>
2021-09-09 19:49:25 +0800

    Update HttpRequestUtil.java

--- src/main/java/com/suimz/example/api/signature/sign/util/HttpRequestUtil.java
+++ src/main/java/com/suimz/example/api/signature/sign/util/HttpRequestUtil.java
@@ -25,19 +25,19 @@ public class HttpRequestUtil {
      */
     public static SortedMap<String, String> getAllParams(HttpServletRequest request) throws IOException {
         SortedMap<String, String> result = new TreeMap<>();
-        // 获取URL上的参数
-        Map<String, String> urlParams = getUrlParams(request);
+        // 获取 URL 上的参数
+        Map<String, Object> urlParams = getUrlParams(request);
         for (Map.Entry entry : urlParams.entrySet()) {
             result.put((String) entry.getKey(), String.valueOf(entry.getValue()));
         }
-        Map<String, String> allRequestParam = new HashMap<>(16);
-        // get请求不需要拿body参数
+        // 获取 body 中的参数
+        Map<String, String> bodyParams = new HashMap<>();
         if (!HttpMethod.GET.name().equals(request.getMethod())) {
-            allRequestParam = getAllRequestParam(request);
+            bodyParams = geBodyParam(request);
         }
-        // 将URL的参数和body参数进行合并
-        if (allRequestParam != null) {
-            for (Map.Entry entry : allRequestParam.entrySet()) {
+        // 将 URL 的参数和 body 参数进行合并
+        if (ObjectUtil.isNotEmpty(bodyParams)) {
+            for (Map.Entry entry : bodyParams.entrySet()) {
                 result.put((String) entry.getKey(), String.valueOf(entry.getValue()));
             }
         }
@@ -45,10 +45,10 @@ public class HttpRequestUtil {
     }
 
     /**
-     * 获取 Body 参数 - json格式
+     * 获取 Body 中的参数（json），转换成 Map
      * @param request
      */
-    public static Map<String, String> getAllRequestParam(final HttpServletRequest request) throws IOException {
+    public static Map<String, Object> geBodyParam(final HttpServletRequest request) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
         String str = "";
         StringBuilder wholeStr = new StringBuilder();
@@ -61,16 +61,16 @@ public class HttpRequestUtil {
     }
 
     /**
-     * 将URL请求参数转换成Map
+     * 获取 URL 上的参数，转换成 Map
      * @author show
      * @param request
      */
-    public static Map<String, String> getUrlParams(HttpServletRequest request) {
+    public static Map<String, Object> getUrlParams(HttpServletRequest request) {
         String param = "";
         try {
             param = URLDecoder.decode(request.getQueryString(), "utf-8");
         } catch (Exception e) { }
-        Map<String, String> result = new HashMap<>(16);
+        Map<String, Object> result = new HashMap<>();
         if (ObjectUtil.isNotEmpty(param)) {
             String[] params = param.split("&");
             for (String s : params) {
